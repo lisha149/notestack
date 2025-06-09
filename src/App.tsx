a0c8818c7763e648fce50031b7fc4a25e3f3cfdf
Palisha Shakya <palishashakya@gmail.com>
2025-06-09 14:37:14 +0545

    feat: project setup for notestack

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { LogoIcon } from "./assets/svg";
+
+function App() {
+  return (
+    <>
+      <LogoIcon />
+      <h1>NoteStack</h1>
+      <h4>Stack your thoughts, where ideas stay organized.</h4>
+    </>
+  );
+}
+
+export default App;
